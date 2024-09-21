31c8d19e9c8f60c2478737123e34cfeaa6edaceb
kloeffler95 <kevin.loeffler@colorado.edu>
2024-09-21 11:35:56 -0600

    Setting up manifest.json and updating README.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# WXT + React
+# LegiBrowse
 
 This template should help get you started developing with React in WXT.
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,18 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    manifest_version: 3,
+    name: "LegiBrowse",
+    version: "1.0",
+    description: "A browser extension that helps low-vision users by replacing fonts in webpages where possible with a hyperlegible font. ",
+    permissions: ["activeTab"],
+    content_scripts: [
+      {
+        "matches": ["<all_urls>"],
+        "js": ["contentScript.js"],
+        "run_at": "document_end"
+      }
+    ]
+    },
 });
