540ca35b5f534384493922c98d7084e632f149d2
kloeffler95 <kevin.loeffler@colorado.edu>
2024-09-22 12:33:03 -0600

    Applying transparency.

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
 import './App.css';
 
 function App() {
@@ -9,9 +8,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
